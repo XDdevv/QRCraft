96726681899cb03ed4bf2726b29216e23c48f0d6
XDdevv <undefineduser087@gmail.com>
2025-08-11 11:50:00 +0500

    Fixed line heights in app typography

--- app/src/main/java/zed/rainxch/qrcraft/core/presentation/design_system/theme/Type.kt
+++ app/src/main/java/zed/rainxch/qrcraft/core/presentation/design_system/theme/Type.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import zed.rainxch.qrcraft.R
 
@@ -19,24 +20,24 @@ val Typography = Typography(
         fontFamily = suse_font,
         fontWeight = FontWeight.SemiBold,
         fontSize = 28.sp,
-        lineHeight = 32.sp
+        lineHeight = 1.1.em
     ),
     titleSmall = TextStyle(
         fontFamily = suse_font,
         fontWeight = FontWeight.SemiBold,
         fontSize = 19.sp,
-        lineHeight = 24.sp
+        lineHeight = 1.1.em
     ),
     labelLarge = TextStyle(
         fontFamily = suse_font,
         fontWeight = FontWeight.Medium,
-        lineHeight = 20.sp,
+        lineHeight = 1.1.em,
         fontSize = 16.sp
     ),
     bodyLarge = TextStyle(
         fontFamily = suse_font,
         fontWeight = FontWeight.Normal,
-        lineHeight = 16.sp,
+        lineHeight = 1.1.em,
         fontSize = 20.sp
     ),
 )
