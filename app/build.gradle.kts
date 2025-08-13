2c5dba13bdc3257f234fc89a20cc53dd57380fd5
XDdevv <undefineduser087@gmail.com>
2025-08-13 10:12:22 +0500

    Added kt-serialization + camerax + camerax mlkit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.2.0"
 }
 
 android {
@@ -76,12 +77,20 @@ dependencies {
     implementation(libs.qr.scanner.plugin)
 
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+
     // Permissions
     implementation(libs.accompanist.permissions)
 
     implementation(libs.barcode.scanning)
 
     implementation(libs.androidx.core.splashscreen)
+
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.mlkit.vision)
 
     // Unit testing
     testImplementation(libs.junit)
